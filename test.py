43778ce89ad42c8ff64f00fdec0ff7c7c81db2f0
Aida-Larhrib <alarhrib@tudelft.nl>
2024-10-11 16:06:17 +0200

    Here I found a function that works that lets me 'zoom' into the area I am interested in. So I think this is the final version of the code for the midterm.

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 from cartopy.geodesic import Geodesic
+import shapely.geometry as sgeom
 import matplotlib.patches as mpatches
 import cartopy.feature as cfeature
 
-#draw out the map and chose the variable that I want to be in there:
+# Creating a cartopy, choosing the features I want to be shown on it:
 ax = plt.axes(projection=ccrs.PlateCarree())
 ax.coastlines()
 ax.add_feature(cfeature.NaturalEarthFeature('physical', 'land', '50m', edgecolor='black', facecolor=cfeature.COLORS['land']))
@@ -13,19 +14,23 @@ ax.add_feature(cfeature.NaturalEarthFeature('physical', 'lakes', '50m', edgecolo
 ax.add_feature(cfeature.NaturalEarthFeature('physical', 'rivers_lake_centerlines', '50m', edgecolor=cfeature.COLORS['water'], facecolor='none'))
 ax.add_feature(cfeature.NaturalEarthFeature('cultural', 'admin_1_states_provinces_lines', '50m', edgecolor='gray', facecolor='none'))
 
+# Save the plot by calling plt.savefig() BEFORE plt.show()
 plt.savefig('coastlines.pdf')
 plt.savefig('coastlines.png')
 
-#giving values to the lat and the lon as well as the radius: 
+# Coordinates for the center of the circle
 longitude = 4.3556
-latitude = 52.00667
+latitude =52.00667 
 r = 0.15
 
-#Plotting the circle
-ax.add_patch(mpatches.Circle(xy=[longitude, latitude], radius=r, color='red', alpha=0.3, transform=ccrs.PlateCarree(), zorder=30))
 
-#This defines the bounding box around the coordinates
-zoom_radius = 2  # degrees to zoom in
-ax.set_extent([longitude - zoom_radius, longitude + zoom_radius, latitude - zoom_radius, latitude + zoom_radius], crs=ccrs.PlateCarree())
+#generate the circle
+ax.add_patch(mpatches.Circle(xy=[longitude, latitude], radius=r, color='red', alpha=0.3, transform=ccrs.PlateCarree(), zorder=30))
+plt.savefig('CircleTest.png')
+
+
+# Add coastlines for reference
+ax.coastlines()
+
 
 plt.show()
