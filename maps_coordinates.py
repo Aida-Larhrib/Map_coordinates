cba2f35f03f3ab5a7cf79834a32636edbae94f39
Aida-Larhrib <alarhrib@tudelft.nl>
2024-10-11 16:01:47 +0200

    Now this code works for any given coordinate, the issue is that you have to zoom in quite a bit to see the cricle

--- maps_coordinates.py
+++ maps_coordinates.py
@@ -1,28 +1,30 @@
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 from cartopy.geodesic import Geodesic
-import pandas as pd
 import matplotlib.patches as mpatches
 import cartopy.feature as cfeature
 
 #draw out the map and chose the variable that I want to be in there:
 ax = plt.axes(projection=ccrs.PlateCarree())
 ax.coastlines()
-ax.gridlines()
-ax.add_feature(cfeature.LAND)
-ax.add_feature(cfeature.OCEAN)
+ax.add_feature(cfeature.NaturalEarthFeature('physical', 'land', '50m', edgecolor='black', facecolor=cfeature.COLORS['land']))
+ax.add_feature(cfeature.NaturalEarthFeature('cultural', 'admin_0_countries', '50m', edgecolor='black', facecolor='none'))
+ax.add_feature(cfeature.NaturalEarthFeature('physical', 'lakes', '50m', edgecolor='none', facecolor=cfeature.COLORS['water']), alpha=0.5)
+ax.add_feature(cfeature.NaturalEarthFeature('physical', 'rivers_lake_centerlines', '50m', edgecolor=cfeature.COLORS['water'], facecolor='none'))
+ax.add_feature(cfeature.NaturalEarthFeature('cultural', 'admin_1_states_provinces_lines', '50m', edgecolor='gray', facecolor='none'))
 
-#giving values to the lat and the lon: 
-longitude = 52.00667
-latitude = 4.3556
-radius = 0.15
+plt.savefig('coastlines.pdf')
+plt.savefig('coastlines.png')
+
+#giving values to the lat and the lon as well as the radius: 
+longitude = 4.3556
+latitude = 52.00667
+r = 0.15
 
 #Plotting the circle
 ax.add_patch(mpatches.Circle(xy=[longitude, latitude], radius=r, color='red', alpha=0.3, transform=ccrs.PlateCarree(), zorder=30))
-ax.set_xlim([-latitude-5, latitude+5])
-ax.set_ylim([-longitude-5, longitude+5])
+#ax.set_xlim([-latitude-5, latitude+5])
+#ax.set_ylim([-longitude-5, longitude+5])
 
 #plt. save and show the figure
-plt.savefig('coastlines.pdf')
-plt.savefig('coastlines.png')
 plt.show()
