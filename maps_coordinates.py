6b9bc772deb996b8884e48232c0849e5433df42f
Aida-Larhrib <alarhrib@tudelft.nl>
2024-10-21 16:47:40 +0200

    Divided the project in three functions: one to generate the map, one to draw a circle at the input coordinates and one to 'zoom' into the map at the coordintes of interest.

--- maps_coordinates.py
+++ maps_coordinates.py
@@ -5,27 +5,33 @@ import matplotlib.patches as mpatches
 import cartopy.feature as cfeature
 
 #draw out the map and chose the variable that I want to be in there:
-ax = plt.axes(projection=ccrs.PlateCarree())
-ax.coastlines()
-ax.add_feature(cfeature.NaturalEarthFeature('physical', 'land', '50m', edgecolor='black', facecolor=cfeature.COLORS['land']))
-ax.add_feature(cfeature.NaturalEarthFeature('cultural', 'admin_0_countries', '50m', edgecolor='black', facecolor='none'))
-ax.add_feature(cfeature.NaturalEarthFeature('physical', 'lakes', '50m', edgecolor='none', facecolor=cfeature.COLORS['water']), alpha=0.5)
-ax.add_feature(cfeature.NaturalEarthFeature('physical', 'rivers_lake_centerlines', '50m', edgecolor=cfeature.COLORS['water'], facecolor='none'))
-ax.add_feature(cfeature.NaturalEarthFeature('cultural', 'admin_1_states_provinces_lines', '50m', edgecolor='gray', facecolor='none'))
+def map_generation(ax=None):
+    if ax is None : 
+        ax = plt.axes(projection=ccrs.PlateCarree())
+    ax.coastlines()
+    ax.add_feature(cfeature.NaturalEarthFeature('physical', 'land', '50m', edgecolor='black', facecolor=cfeature.COLORS['land']))
+    ax.add_feature(cfeature.NaturalEarthFeature('cultural', 'admin_0_countries', '50m', edgecolor='black', facecolor='none'))
+    ax.add_feature(cfeature.NaturalEarthFeature('physical', 'lakes', '50m', edgecolor='none', facecolor=cfeature.COLORS['water']), alpha=0.5)
+    ax.add_feature(cfeature.NaturalEarthFeature('physical', 'rivers_lake_centerlines', '50m', edgecolor=cfeature.COLORS['water'], facecolor='none'))
+    ax.add_feature(cfeature.NaturalEarthFeature('cultural', 'admin_1_states_provinces_lines', '50m', edgecolor='gray', facecolor='none'))
+    return ax
 
 plt.savefig('coastlines.pdf')
 plt.savefig('coastlines.png')
 
-#giving values to the lat and the lon as well as the radius: 
-longitude = -7.603869
-latitude = 33.589886
-r = 0.15
-
-#Plotting the circle
-ax.add_patch(mpatches.Circle(xy=[longitude, latitude], radius=r, color='red', alpha=0.3, transform=ccrs.PlateCarree(), zorder=30))
+#Defining a function for coordinates and radius of the circle that is to be mapped
+def plot_circle(ax, longitude, latitude, r):
+    longitude = -7.603869
+    latitude = 33.589886
+    r = 0.15
+    circle = ax.add_patch(mpatches.Circle(xy=[longitude, latitude], radius=r, color='red', alpha=0.3, transform=ccrs.PlateCarree(), zorder=30))
+    return circle
 
 #This defines the bounding box around the coordinates
-zoom_radius = 5  # degrees to zoom in
-ax.set_extent([longitude - zoom_radius, longitude + zoom_radius, latitude - zoom_radius, latitude + zoom_radius], crs=ccrs.PlateCarree())
+
+def zoom(ax, degree, longitude, latitude):
+    zoom_radius = 5
+    frame = ax.set_extent([longitude - degree, longitude + zoom_radius, latitude - zoom_radius, latitude + zoom_radius], crs=ccrs.PlateCarree())
+    return frame
 
 plt.show()
